1cec6a9d416b2c697cf8a4efabe9eacc4ad210a2
ngworks919 <20eg105259@anurag.edu.in>
2023-02-20 17:26:27 +0530

    first commit

---
+++ src/test/java/com/nithin/app/Flightware1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nithin.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Flightware1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
